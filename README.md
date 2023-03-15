89ce3409d1c4dcf7518bd4178239cea76183c114
Dereck Z <zavaladereck77@gmail.com>
2023-03-14 21:30:28 -0600

    Ending section 7 of NestJS course.

---
+++ README.md
@@ -0,0 +1,30 @@
+<p align="center">
+  <a href="http://nestjs.com/" target="blank"><img src="https://nestjs.com/img/logo-small.svg" width="200" alt="Nest Logo" /></a>
+</p>
+
+# Ejecutar en desarrollo
+
+1. Clonar el repo
+2. Ejecutar:
+
+```
+npm install
+```
+
+3. Tener Nest CLI instalado
+
+```
+npm i -g @nestjs/cli
+```
+
+4. Levantar la bdd
+
+```
+docker-compose up -d
+```
+
+## Stack usado
+
+- MongoDB
+- NestJS
+- Docker
---
+++ src/app.module.ts
@@ -0,0 +1,20 @@
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { PokemonModule } from './pokemon/pokemon.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Module } from '@nestjs/common';
+import { join } from 'path';
+import { CommonModule } from './common/common.module';
+
+@Module({
+  imports: [
+    // exponer la carpeta public
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+    }),
+    // referencia a la bdd
+    MongooseModule.forRoot('mongodb://localhost:27017/nest-pokemon'),
+    PokemonModule,
+    CommonModule,
+  ],
+})
+export class AppModule {}
